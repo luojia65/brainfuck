44071c8ddb7a110ee47fa86eaf9aa0458c9183e4
luojia65 <me@luojia.cc>
2018-06-27 16:38:08 +0800

    Not so good as simulation :P

--- benches/hello_world.rs
+++ benches/hello_world.rs
@@ -24,23 +24,120 @@ impl io::Read for BlackHole {
     }
 }
 
-static PROGRAM: &str = "++++++++++[>+>+++>+++++++>++++++++++<<<<-]>>>++.>+.+++++++..+++.<<++.>+++++++++++++++.>.+++.------.--------.<<+.<.";
+static HELLO_WORLD: &str = "++++++++++[>+>+++>+++++++>++++++++++<<<<-]>>>++.>+.+++++++..+++.<<++.>+++++++++++++++.>.+++.------.--------.<<+.<.";
+static HELLO_WORLD_N: &str = "++++++++++[>+>+++>+++++++>++++++++++<<<<-]>>>++>+++++++++++<<++>+++++++++++++++>+++--------------<<+<";
 
 #[bench]
 fn hello_world_compile(bencher: &mut Bencher) {
     bencher.iter(|| {
-        let _ = test::black_box(PROGRAM).into_program();
+        let _ = test::black_box(HELLO_WORLD).into_program();
     });
 }
 
 #[bench]
 fn hello_world_execute(bencher: &mut Bencher) {
     bencher.iter(|| {
-        let mut process = Process::new(PROGRAM, 1024, BlackHole, BlackHole);
+        let mut process = Process::new(HELLO_WORLD, 1024, BlackHole, BlackHole);
         process.execute().unwrap();
     });
 }
 
+#[bench]
+fn hello_world_execute_no_output(bencher: &mut Bencher) {
+    bencher.iter(|| {
+        let mut process = Process::new(HELLO_WORLD_N, 1024, BlackHole, BlackHole);
+        process.execute().unwrap();
+    });
+}
+
+macro_rules! write_bb {
+    () => {
+        let mut bh = BlackHole;
+        bh.write(test::black_box(&[0u8])).unwrap();
+    };
+}
+
+#[bench]
+fn simulated_hello_world(bencher: &mut Bencher) {
+    use std::io::Write;
+    bencher.iter(|| {
+        let mut m = [0u8; 10];
+        let mut mp = 0;
+
+        m[mp] = m[mp] + 1; m[mp] = m[mp] + 1;
+        m[mp] = m[mp] + 1; m[mp] = m[mp] + 1;
+        m[mp] = m[mp] + 1; m[mp] = m[mp] + 1;
+        m[mp] = m[mp] + 1; m[mp] = m[mp] + 1;
+        m[mp] = m[mp] + 1; m[mp] = m[mp] + 1;
+        while m[mp] != 0 {
+            mp = mp + 1;
+            m[mp] = m[mp] + 1;
+            mp = mp + 1;
+            m[mp] = m[mp] + 1;
+            m[mp] = m[mp] + 1;
+            m[mp] = m[mp] + 1;
+            mp = mp + 1;
+            m[mp] = m[mp] + 1; m[mp] = m[mp] + 1;
+            m[mp] = m[mp] + 1; m[mp] = m[mp] + 1;
+            m[mp] = m[mp] + 1; m[mp] = m[mp] + 1;
+            m[mp] = m[mp] + 1;
+            mp = mp + 1;
+            m[mp] = m[mp] + 1; m[mp] = m[mp] + 1;
+            m[mp] = m[mp] + 1; m[mp] = m[mp] + 1;
+            m[mp] = m[mp] + 1; m[mp] = m[mp] + 1;
+            m[mp] = m[mp] + 1; m[mp] = m[mp] + 1;
+            m[mp] = m[mp] + 1; m[mp] = m[mp] + 1;
+            mp = mp - 1; mp = mp - 1;
+            mp = mp - 1; mp = mp - 1;
+            m[mp] = m[mp] - 1;
+        }
+        m[mp] = m[mp] + 1; m[mp] = m[mp] + 1;
+        mp = mp + 1;
+        m[mp] = m[mp] + 1;m[mp] = m[mp] + 1;
+        write_bb!();
+        mp = mp + 1;
+        m[mp] = m[mp] + 1;
+        write_bb!();
+        m[mp] = m[mp] + 1;m[mp] = m[mp] + 1;
+        m[mp] = m[mp] + 1;m[mp] = m[mp] + 1;
+        m[mp] = m[mp] + 1;m[mp] = m[mp] + 1;
+        m[mp] = m[mp] + 1;
+        write_bb!();
+        write_bb!();
+        m[mp] = m[mp] + 1;m[mp] = m[mp] + 1;
+        m[mp] = m[mp] + 1;
+        write_bb!();
+        mp = mp - 1;mp = mp - 1;
+        m[mp] = m[mp] + 1; m[mp] = m[mp] + 1;
+        write_bb!();
+        mp = mp + 1;
+        m[mp] = m[mp] + 1; m[mp] = m[mp] + 1; m[mp] = m[mp] + 1; m[mp] = m[mp] + 1; m[mp] = m[mp] + 1;
+        m[mp] = m[mp] + 1; m[mp] = m[mp] + 1; m[mp] = m[mp] + 1; m[mp] = m[mp] + 1; m[mp] = m[mp] + 1;
+        m[mp] = m[mp] + 1; m[mp] = m[mp] + 1; m[mp] = m[mp] + 1; m[mp] = m[mp] + 1; m[mp] = m[mp] + 1;
+        write_bb!();
+        mp = mp + 1;
+        write_bb!();
+        m[mp] = m[mp] + 1; m[mp] = m[mp] + 1;
+        m[mp] = m[mp] + 1;
+        write_bb!();
+        m[mp] = m[mp] - 1; m[mp] = m[mp] - 1;
+        m[mp] = m[mp] - 1; m[mp] = m[mp] - 1;
+        m[mp] = m[mp] - 1; m[mp] = m[mp] - 1;
+        write_bb!();
+        m[mp] = m[mp] - 1; m[mp] = m[mp] - 1;
+        m[mp] = m[mp] - 1; m[mp] = m[mp] - 1;
+        m[mp] = m[mp] - 1; m[mp] = m[mp] - 1;
+        m[mp] = m[mp] - 1;
+        write_bb!();
+        mp = mp - 1;
+        mp = mp - 1;
+        m[mp] = m[mp] + 1;
+        write_bb!();
+        mp = mp - 1;
+        write_bb!();
+        std::mem::drop(mp);
+    });
+}
 
 #[bench]
 fn u32_xor_u32_x1000(bencher: &mut Bencher) {
