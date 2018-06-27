4c265bb079dc2a2a84ffc59720352f3b68bdbdfa
luojia65 <me@luojia.cc>
2018-06-27 16:16:41 +0800

    Reformat & rearrange

--- src/lib.rs
+++ src/lib.rs
@@ -3,7 +3,10 @@ mod op;
 mod process;
 mod program;
 
-pub use error::*;
-pub use op::*;
-pub use process::*;
-pub use program::*;pub mod prelude {}
+pub mod prelude {
+    pub use error::*;
+    pub use op::*;
+    pub use process::*;
+    pub use program::*;
+
+}
